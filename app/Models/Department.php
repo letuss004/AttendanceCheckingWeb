da0cdb692865994422397d5e40f39890d691f988
letuss004 <letuss004@gmail.com>
2021-12-13 14:19:46 +0700

    Set up laravel migrate: simple set up. User, teacher , admin, department

---
+++ app/Models/Department.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Department
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Department newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Department newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Department query()
+ * @mixin \Eloquent
+ */
+class Department extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/UserType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\UserType
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|UserType newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserType newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserType query()
+ * @mixin \Eloquent
+ */
+class UserType extends Model
+{
+    use HasFactory;
+}
